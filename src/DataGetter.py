af18c70df103bbe6d8820374bd96f871b716f35c
konradquam <konradquam@gmail.com>
2024-01-09 21:59:59 -0600

    refactored functions for connecting to database and querying

--- src/DataGetter.py
+++ src/DataGetter.py
@@ -8,13 +8,16 @@ import psycopg2
 api_key_path = '../nasdaq_api_key.txt'
 db_config_path = '../db_config.json'
 
+#cursor for database connection
+cursor = None
+
 def from_api(start_date, end_date, api_key_path='../nasdaq_api_key.txt'):
     '''
     get data from nasdaq data link (tables api)
     :param start_date:
     :param end_date:
     :param api_key_path: filepath to api_key file
-    :return: data (pandas dataframe
+    :return: data (pandas dataframe)
     '''
     # read api_key for nasdaq data link
     api_key = Key_Getter.get_api_key(api_key_path)
@@ -31,26 +34,26 @@ def conn_db(db_config_path=db_config_path):
     '''
     Connects to database
     :param db_config_path: filepath to db config file
-    :return: cursor for executing queries
+    :return: None
     '''
+    global cursor
+
     db_config = JSON_Reader.read_json(db_config_path)
     db_conn = psycopg2.connect(database=db_config['database'], host=db_config['host'], user=db_config['username'], password=db_config['password'], port=db_config['port'])
-    return db_conn.cursor()
+    cursor = db_conn.cursor()
 
-def insert_db(cursor, data):
+def insert_db(data):
     '''
     Inserts data into database
-    :param cursor: executes queries (use from conn_db),
     :param data: data to insert (pandas dataframe)
-    :return: nothing
+    :return: None
     '''
     for i in range(data.shape[0]):
         cursor.execute('INSERT INTO opec_prices (price_date, price) VALUES (%s, %s)', (data.loc[i, "date"], data.loc[i, "price"]))
 
-def get_db(cursor, start_date, end_date):
+def get_db(start_date, end_date):
     '''
     Retrieves data from database
-    :param cursor: executes queries (use from conn_db)
     :param start_date:
     :param end_date:
     :return: data (pandas dataframe)
@@ -61,12 +64,11 @@ def get_db(cursor, start_date, end_date):
     data['price'] = data['price'].astype(float)
     return data
 
-def delete_db(cursor, start_date, end_date):
+def delete_db(start_date, end_date):
     '''
     Deletes rows from database opec_prices table
-    :param cursor: executes delete statement
     :param start_date:
     :param end_date:
-    :return: Nothing
+    :return: None
     '''
     cursor.execute(f'DELETE FROM opec_prices WHERE price_date >= %s AND price_date <= %s', (start_date, end_date))
--- tests/db_tester.py
+++ tests/db_tester.py
@@ -9,7 +9,7 @@ class db_tester(unittest.TestCase):
     Test retrieving data
     '''
 
-    def test_insert_retrieve(self):
+    def test_insert_retrieve_delete(self):
         '''
         Test connecting to database,
         Test inserting data,
@@ -18,7 +18,7 @@ class db_tester(unittest.TestCase):
         ----
         All in one function because I need to retrieve inserted data to ensure correct data was inserted,
         I also need to delete data so I don't leave fake data in db
-        :return: Nothing
+        :return: None
         '''
         start_date = '3000-01-01'
         end_date = '3000-01-03'
@@ -26,26 +26,26 @@ class db_tester(unittest.TestCase):
         ins_data['date'] = ins_data['date'].astype(str)
         ins_data['price'] = ins_data['price'].astype(float)
 
-        cursor = DataGetter.conn_db()
-        DataGetter.insert_db(cursor=cursor, data=ins_data)
+        DataGetter.conn_db()
+        DataGetter.insert_db(ins_data)
 
         # retrieved all the inserted data, should be equal
-        retrieved_data = DataGetter.get_db(cursor, start_date, end_date)
+        retrieved_data = DataGetter.get_db(start_date, end_date)
         self.assertTrue(ins_data.equals(retrieved_data))
 
         # retrieved only one row of the inserted data, should not be equal
-        retrieved_data = DataGetter.get_db(cursor, start_date, start_date)
+        retrieved_data = DataGetter.get_db(start_date, start_date)
         self.assertTrue(not ins_data.equals(retrieved_data))
 
         # retrieving data not in db, should not be equal
         # retrieved data is an empty dataframe
-        retrieved_data = DataGetter.get_db(cursor, '3000-02-01', '3000-02-03')
+        retrieved_data = DataGetter.get_db('3000-02-01', '3000-02-03')
         self.assertTrue(not ins_data.equals(retrieved_data))
         self.assertTrue(retrieved_data.empty)
 
         # delete data, should not be equal
         # retrieved data is an empty dataframe
-        DataGetter.delete_db(cursor, start_date, end_date)
-        retrieved_data = DataGetter.get_db(cursor, start_date, end_date)
+        DataGetter.delete_db(start_date, end_date)
+        retrieved_data = DataGetter.get_db(start_date, end_date)
         self.assertTrue(not ins_data.equals(retrieved_data))
         self.assertTrue(retrieved_data.empty)
